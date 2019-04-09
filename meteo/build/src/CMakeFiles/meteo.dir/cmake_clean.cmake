5ced451e82d48a42bf1e2a0c5bad70555eda52e2
Alexia31200 <alexia.turines.pro@gmail.com>
2019-04-09 16:51:25 +0200

    ajout projet

---
+++ meteo/build/src/CMakeFiles/meteo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "meteo_autogen"
+  "CMakeFiles/meteo_autogen.dir/AutogenOldSettings.cmake"
+  "qrc_resources.cpp"
+  "CMakeFiles/meteo.dir/main.cpp.o"
+  "CMakeFiles/meteo.dir/qrc_resources.cpp.o"
+  "CMakeFiles/meteo.dir/meteo_autogen/mocs_compilation.cpp.o"
+  "meteo.pdb"
+  "meteo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/meteo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
